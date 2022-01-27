17f2d41e5354f31d5a65e0264b719dac83c18256
seabothsides <youngserge1@gmail.com>
2022-01-27 23:11:03 +0000

    styles: updated nav bar for desktop and tablet

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,9 +1,50 @@
 <template>
-  <nav class="bg-gray-200 md:px-56 md:py-1.5 md:grid grid-cols-2 items-center">
-    <nuxt-link class="font-bold text-3xl hover:text-yellow-400" to="/"
+  <nav
+    class="
+      bg-gray-200
+      px-3
+      md:px-8
+      lg:px-56
+      md:py-1.5
+      flex
+      justify-between
+      lg:grid
+      grid-cols-2
+      items-center
+    "
+  >
+    <nuxt-link
+      class="font-bold text-2xl md:text-3xl hover:text-yellow-400"
+      to="/"
       >Brand Logo</nuxt-link
     >
-    <div class="md:flex md:justify-between capitalize">
+    <div class="flex space-x-1 md:space-x-3 lg:space-x-7 capitalize">
+      <nuxt-link
+        class="
+          transition
+          ease-in-out
+          duration-200
+          text-lg
+          font-semibold
+          hover:text-yellow-400 hover:underline
+        "
+        v-for="link in links"
+        :to="{ name: link.link }"
+        :key="link.id"
+      >
+        {{ link.name }}</nuxt-link
+      >
+    </div>
+    <div
+      class="
+        grid grid-auto-row
+        md:hidden
+        z-20
+        md:space-x-3
+        lg:space-x-7
+        capitalize
+      "
+    >
       <nuxt-link
         class="
           transition
@@ -41,16 +82,16 @@ export default {
           link: 'results',
         },
         {
+          name: 'blog',
+          link: 'blog',
+        },
+        {
           name: 'community',
           link: 'community',
         },
         {
           name: 'shop',
           link: 'shop',
-        },
-        {
-          name: 'blog',
-          link: 'blog',
         },
       ],
     }
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <nav-bar />
-    <nuxt class="px-4 md:px-60 md:pt-4 m-0" />
+    <nuxt class="px-4 md:px-28 lg:px-56 md:pt-4 m-0" />
   </div>
 </template>
 
