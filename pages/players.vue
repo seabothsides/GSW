332cc823b505cfdabdf7c9a6be558ba7b45cdb68
seabothsides <youngserge1@gmail.com>
2022-01-31 00:18:52 +0000

    styles: players images loading and colored titles on player page, responsiveness added to players page

--- pages/players.vue
+++ pages/players.vue
@@ -10,15 +10,44 @@ export default {
 
 <template>
   <div>
-    <h2>Some title</h2>
-    <div v-for="(player, index) in players.data" :key="index">
-      <p>
-        {{ player.name }}
-      </p>
-      <p>
-        {{ player.number }}
-      </p>
+    <h2
+      class="
+        capitalize
+        pb-2
+        text-4xl
+        font-bold
+        italic
+        bg-clip-text bg-gradient-to-br
+        from-yellow-400
+        to-pink-400
+        text-transparent
+      "
+    >
+      all the mighty warriors
+    </h2>
+    <div class="grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-5 pb-8">
+      <div
+        v-for="(player, index) in players.data"
+        :key="index"
+        class="rounded-lg grid w-full grid place-items-center"
+      >
+        <p class="capitalize font-semibold text-2xl text-center">
+          {{ player.name }}
+        </p>
+        <p
+          class="
+            text-2xl
+            font-black
+            bg-clip-text bg-gradient-to-br
+            from-yellow-400
+            to-pink-500
+            text-transparent
+          "
+        >
+          {{ player.number }}
+        </p>
+        <img :src="player.img" alt="" class="w-80 rounded-3xl" />
+      </div>
     </div>
-    <p>{{ players }}</p>
   </div>
 </template>
