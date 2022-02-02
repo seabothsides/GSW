5a472ec1a4b5a7b3a73e04f717ca03344a997e53
seabothsides <youngserge1@gmail.com>
2022-02-02 17:01:28 +0000

    functions: nav bar mobile menu integration. bug: about page displays above nav bar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,50 +1,76 @@
 <template>
-  <nav
-    class="
-      bg-gray-200
-      px-3
-      md:px-8
-      lg:px-56
-      md:py-1.5
-      flex
-      justify-between
-      lg:grid
-      grid-cols-2
-      items-center
-    "
-  >
-    <p>
-      <nuxt-link
-        class="font-bold text-2xl md:text-3xl hover:text-yellow-400"
-        to="/"
-        >Brand Logo</nuxt-link
-      >
-    </p>
-    <div class="flex space-x-1 md:space-x-3 lg:space-x-7 capitalize">
-      <nuxt-link
+  <div>
+    <nav
+      class="
+        bg-gray-200
+        px-3
+        py-1.5
+        md:px-8 md:py-1.5
+        flex
+        justify-between
+        lg:grid lg:px-32
+        xl:px-56
+        grid-cols-2
+        items-center
+        sm:bg-purple-300
+        md:bg-green-300
+        lg:bg-red-200
+        xl:bg-blue-300
+      "
+    >
+      <p>
+        <nuxt-link
+          class="font-bold text-2xl md:text-3xl hover:text-yellow-400"
+          to="/"
+          >Brand Logo</nuxt-link
+        >
+      </p>
+      <div
         class="
-          transition
-          ease-in-out
-          duration-200
-          text-lg
-          font-semibold
-          hover:text-yellow-400 hover:underline
+          flex
+          space-x-1
+          sm:space-x-4
+          md:space-x-5
+          lg:space-x-7
+          xl:space-x-12
+          capitalize
+          hidden
+          sm:flex
         "
-        v-for="link in links"
-        :to="{ name: link.link }"
-        :key="link.id"
       >
-        {{ link.name }}</nuxt-link
-      >
-    </div>
+        <nuxt-link
+          class="
+            transition
+            ease-in-out
+            duration-200
+            text-lg
+            font-semibold
+            hover:text-yellow-400 hover:underline
+          "
+          v-for="link in links"
+          :to="{ name: link.link }"
+          :key="link.id"
+          @click="isOpen"
+        >
+          {{ link.name }}</nuxt-link
+        >
+      </div>
+      <button @click="isOpen">menu</button>
+    </nav>
     <div
+      id="nav-draw"
       class="
         grid grid-auto-row
-        md:hidden
-        z-20
-        md:space-x-3
-        lg:space-x-7
+        hidden
+        absolute
+        bg-gray-500
         capitalize
+        w-full
+        h-full
+        place-content-center
+        transition-all
+        ease-in-out
+        duration-200
       "
     >
       <nuxt-link
@@ -63,7 +89,7 @@
         {{ link.name }}</nuxt-link
       >
     </div>
-  </nav>
+  </div>
 </template>
 
 <script>
@@ -97,6 +123,12 @@ export default {
         },
       ],
     }
+  },
+  methods: {
+    isOpen() {
+      var element = document.getElementById('nav-draw')
+      element.classList.toggle('hidden')
+    },
   },
 }
 </script>
